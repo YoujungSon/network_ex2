4f2ab39131faa51b6b81418cfa863b630ef06606
YoujungSon <andn1026@gmail.com>
2022-06-06 14:15:09 +0900

    network2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const callSomething = async () => {
+    let data = {
+      day: "일",
+      sleep_time: "10:00",
+    };
+    const response = await fetch("http://localhost:5001/sleep_times", {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify(data),
+    });
+
+    console.log(response);
+  };
+
+  const callSomethingAxios = () => {
+    // axios({
+    //   method: "get",
+    //   url: "http://localhost:5001/sleep_times",
+    // }).then((response) => {
+    //   console.log(response);
+    // });
+
+    // axios.get("http://localhost:5001/sleep_times").then((response) => {
+    //   console.log(response);
+    // });
+    let data = {
+      day: "일",
+      sleep_time: "10:00",
+    };
+    axios.post("http://localhost:5001/sleep_times", data).then((response) => {
+      console.log(response);
+    });
+  };
+
+  React.useEffect(() => {
+    // callSomething();
+    callSomethingAxios();
+  });
+  return <div className="App"></div>;
 }
 
 export default App;
